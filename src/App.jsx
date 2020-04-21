323c2f3b073f391050f2b631dd3e75087e536fa7
tamaramunoz <tami.skt@gmail.com>
2020-04-21 16:13:52 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div >
+        <h1>Hola mundo desde redux</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/pokeDucks.js
@@ -0,0 +1,5 @@
+// constants
+
+// reducer
+
+// actions
