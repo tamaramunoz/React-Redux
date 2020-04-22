77ede5d8df300b46976244ef4f1245917e7098b7
tamaramunoz <tami.skt@gmail.com>
2020-04-21 21:48:24 -0400

    pokeDucks config

--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -1,5 +1,33 @@
+import axios from 'axios'
+
 // constants
+const dataInicial = {
+    array : []
+}
+
+// types
+const OBTENER_POKEMONES_EXITO = 'OBTENER_POKEMONES_EXITO'
 
 // reducer
+export default function pokeReducer(state = dataInicial, action){
+    switch(action.type){
+        case OBTENER_POKEMONES_EXITO:
+            return {...state, array: action.payload}
+        default:
+            return state
+    }
+}
 
 // actions
+export const obtenerPokemonesAccion = () => async (dispatch, getState) => {
+    try {
+        const res = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
+        dispatch({
+            type: OBTENER_POKEMONES_EXITO,
+            payload: res.data.results
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+}
